ff1b01f9b38a0ad5d6eff7ac7f18101d23aaec06
Marketing Drasi <brunocalura@discente.ufg.br>
2024-12-03 22:58:48 -0300

    código finalizado

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_login import LoginManager
+
 app = Flask(__name__, template_folder='templates') 
+login_manager = LoginManager(app)
 
 from app import route
 from app import cliente
